fd1a7b3f045d62d53d12e9b787c8d0be0d476c4f
MunthaliAustin <munthaliaustin56@gmail.com>
2024-07-12 12:59:20 +0200

    built the routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Nav from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        <body>{children}</body>
+        <body className="mt-5">
+          <Nav></Nav>
+          {children}
+        </body>
       </UserProvider>
     </html>
   );
