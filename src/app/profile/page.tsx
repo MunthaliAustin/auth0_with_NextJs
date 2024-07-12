f5501146195d45135a2cd8558faf5c1a027476a6
MunthaliAustin <munthaliaustin56@gmail.com>
2024-07-12 16:52:09 +0200

    profile pulse editted

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,7 +12,7 @@ export default function ProfileClient() {
     return (
       <div className="border border-blue-300 shadow rounded-md p-4 mx-11 min-h-screen mt-4">
         <div className="animate-pulse flex space-x-4">
-          <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          {/* <div className="rounded-full bg-slate-700 h-10 w-10"></div> */}
           <div className="flex-1 space-y-6 py-1">
             <div className="h-2 bg-slate-700 rounded"></div>
             <div className="space-y-3">
